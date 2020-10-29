933c511c245a721ef5277b34dd19ed37c8a70ff5
user <user@Jourdans-iMac.lan>
2020-10-29 13:56:01 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react"
 import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Project from "./components/Project";
+import {BrowserRouter as Router, Route, Swtich} from "react-router-dom"
 
 function App() {
   return (
